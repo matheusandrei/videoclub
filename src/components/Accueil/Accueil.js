b761430d57390245f2328dbd3bca81736e7dbcbb
Matheus <matheus@hotmail.ca>
2024-05-30 17:32:04 -0400

    englober contenu accueil dans wrapper

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -2,11 +2,13 @@ import accueilData from './Accueil.json';
 
 function Accueil() {
   return (
-    <div>
-      <h2>Accueil</h2>
-      {accueilData.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))}
+    <div class="wrapper">
+      <main>
+        <h2>Accueil</h2>
+        {accueilData.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </main>
     </div>
   );
 }
