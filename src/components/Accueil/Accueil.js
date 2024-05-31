92c68018acb40ec9970f08628690b811dfcfea1d
Matheus <matheus@hotmail.ca>
2024-05-31 17:45:11 -0400

    changer style et banner pour accueil, ajouter link vercel dans le readme

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -2,18 +2,16 @@ import accueilData from './Accueil.json';
 import "./Accueil.css";
 function Accueil() {
   return (
-    <>
-    <div class="image-container">
-      <img class="hero" src="/img/hero-banner.jpg" alt="hero"/>
+<>
+  <div className="image-container">
+    <img className="hero" src="/img/hero-banner.jpg" alt="hero" />
+    <div className="text-overlay">
+      {accueilData.map((paragraph, index) => (
+        <p className="accueil-p" key={index}>{paragraph}</p>
+      ))}
     </div>
-    <div className="wrapper">
-      <main>
-        {accueilData.map((paragraph, index) => (
-          <p className="accueil-p" key={index}>{paragraph}</p>
-        ))}
-      </main>
-    </div>
-    </>
+  </div>
+</>
   );
 }
 
